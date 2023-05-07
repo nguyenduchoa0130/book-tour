d71ed6cd80135e05668fc7b801b85c52844f8d54
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-07 15:49:01 +0700

    origin

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
+/*import { Layout, Space } from 'antd';*/
+
+
 
 function App() {
+  const footerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#7dbcea',
+  };
   return (
     <>
       <Header />
@@ -12,8 +20,78 @@ function App() {
       </main>
       <Footer />
     </>
-  );
+  )
 }
-
-export default App;
-
+  export default App
+ /*const { Header, Footer, Sider, Content } = Layout;
+  const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 50,
+    lineHeight: '64px',
+    backgroundColor: '#7dbcea',
+  };
+  
+  const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#108ee9',
+  };
+  
+  const siderStyle: React.CSSProperties = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#3ba0e9',
+  };
+  
+  const footerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#7dbcea',
+  };
+  /*return (
+  <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider style={siderStyle}>Sider</Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Content style={contentStyle}>Content</Content>
+        <Sider style={siderStyle}>Sider</Sider>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Sider style={siderStyle}>Sider</Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
+  </Space>
+);
+  }*/
+ 
+/*function App() {
+  return (
+    <div className="App">
+        <Button type="primary">Hello gà coder</Button>
+      </div>
+);
+}*/
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React from 'react'
 import './styles.css';
+import { Card, FloatButton, List } from 'antd';
+import {LinkedinFilled,FacebookFilled, InstagramFilled, QuestionCircleFilled } from '@ant-design/icons';
+function Footer() {
+  const data = [
+    {
+      title: 'Thông tin về chúng tôi ',  
+    },
+  ]
+  return (
+   <> 
+    <List
+    grid={{ gutter: 30, column: 1 }}
+    dataSource={data}
+    renderItem={(item) => (
+      <List.Item>
+        <Card 
+         style={{right :24,}}// 20-23khong dich sang phai ?
+         title={item.title}>
+         Bạn cần trợ giúp hãy liên lạc với chúng tôi:
+         <p><b>0865081605</b></p>
+        </Card>
+        <FacebookFilled style={{ fontSize: '30px', color :"#4096ff", right: 24}} /> 
+        <LinkedinFilled style={{ fontSize: '30px', color :"#0958d9"}}/>
+        <InstagramFilled style={{ fontSize: '30px', color :"#ff4d4f"}}/>
+        <FloatButton 
+        icon = {<QuestionCircleFilled />}
+        style ={{fontSize: '30px'}}
+        />
+      </List.Item>
+    )}
+  />
+   </>
 
-const Footer = () => {
-  return <footer className='footer'>Footer</footer>;
-};
+    )
+}
 
-export default Footer;
 
+export default Footer
+ //*<FacebookFilled style={{ fontSize: '30px', color :"grid"}} />*//
