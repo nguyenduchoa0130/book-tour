24c8480e3bea1ebb08d9d2cf4f9d5b877dd533b4
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-10 10:25:43 +0700

    origin

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,25 +11,33 @@ function Footer() {
   return (
    <>
    <div className='footer'> 
-     
-      <div>
+
+       <a href ='www.facebook.com'>
+        <FacebookFilled style={{ fontSize: '30px', color :"#4267b2"}} />
+       </a>
+
+        <a href ='www.linkedin.com'>
+        <LinkedinFilled style={{ fontSize: '30px', color :"#0958d9"}}/>
+        </a>
+
+        <a href = 'wwww.instagram.com'>
+        <InstagramFilled style={{ fontSize: '30px', color :"#E95950"}}/>
+        </a>
+
+        <FloatButton 
+          icon = {<QuestionCircleFilled  />}
+          style ={{fontSize: '10px'}}/>
+
         <div classname = 'logo'>
+          <ul> Chứng chỉ kèm theo </ul>
           <img src="//cdn1.ivivu.com/bocongthuong.png" alt ="ivivu"></img>
         </div>
-        <div classname = "contact" style ={{ color : "#faad14"}} > 1900 2045</div>
-        <div>
-          <a href ='www.facebook.com'>
-            <FacebookFilled style={{ fontSize: '30px', color :"#69b1ff"}} />
-          </a>
+        
+        <div classname = "contact">
+          <ul> <b> Bạn cần trợ giúp? Hãy gọi ngay</b></ul>
+          <div style ={{ color : "#faad14", fontSize :"30px"}}>1900 2045 </div>
         </div>
-        <LinkedinFilled style={{ fontSize: '30px', color :"#0958d9"}}/>
-        <InstagramFilled style={{ fontSize: '30px', color :"#cf1322"}}/>
-        <FloatButton 
-        icon = {<QuestionCircleFilled />}
-        style ={{fontSize: '30px'}}
-        />
-      </div>
-    
+ 
   </div>
    </>
   
