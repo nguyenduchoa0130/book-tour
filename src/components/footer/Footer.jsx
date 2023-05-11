9b0bc4c4f6106f7eb425d582004e7176bd19aa22
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-11 19:22:32 +0700

    origin

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,53 +1,103 @@
-import React from 'react'
-import './styles.css';
-import { Card, FloatButton, List } from 'antd';
-import {LinkedinFilled,FacebookFilled, InstagramFilled, QuestionCircleFilled } from '@ant-design/icons';
+import React from "react";
+import "./styles.css";
+import { Card, FloatButton, List } from "antd";
+import {
+  LinkedinFilled,
+  FacebookFilled,
+  InstagramFilled,
+  QuestionCircleFilled,
+} from "@ant-design/icons";
+import { Col, Row } from "antd";
 function Footer() {
-  const data = [
-    {
-      title: 'Thông tin về chúng tôi ',  
-    },
-  ]
   return (
-   <>
-   <div className='footer'> 
-    <div className = 'icons'>
-      <ul>
-       <a href ='www.facebook.com'>
-        <FacebookFilled style={{ fontSize: '30px', color :"#4267b2"}} />
-       </a>
-      
-    
-        <a href ='www.linkedin.com'>
-        <LinkedinFilled style={{ fontSize: '30px', color :"#0958d9"}}/>
-        </a>
-      
-      
-        <a href = 'wwww.instagram.com'>
-        <InstagramFilled style={{ fontSize: '30px', color :"#E95950"}}/>
-        </a>
-      </ul>
-    </div>    
-        <FloatButton 
-          icon = {<QuestionCircleFilled  />}
-          style ={{fontSize: '10px'}}/>
+    <>
+      <div className="footer">
+        <div>
+          <Row>
+            <Col span={12}>
+              col-12
+              <img
+                class="loading"
+                src="https://res.ivivu.com/hotel/img/apea.png"
+              ></img>
+            </Col>
+            <Col span={12}>
+              col-12
+              <img
+                class="loading"
+                src="https://res.ivivu.com/hotel/img/apea.png"
+              ></img>
+            </Col>
+          </Row>
+        </div>
+        <div classname="logo1">
+          <div>
+            <ul>
+              <ul>
+                <b>Được chứng nhận </b>
+              </ul>
+              <img src="//cdn1.ivivu.com/bocongthuong.png" alt="ivivu"></img>
+            </ul>
+            <ul>
+              <img
+                class="loading"
+                src="https://res.ivivu.com/hotel/img/apea.png"
+              ></img>
+              <b>APEA Inspỉational Brand Award</b>
+            </ul>
+          </div>
+        </div>
 
-        <div classname = 'logo'>
-          <ul> Chứng chỉ kèm theo </ul>
-          <img src="//cdn1.ivivu.com/bocongthuong.png" alt ="ivivu"></img>
+        <div className="logo2">
+          <ul>
+            <p>
+              <b>HN: </b> Đại học Bách Khoa Hà Nội{" "}
+            </p>
+            <p>
+              <b>HCM: </b>Lầu 2, Tòa nhà Anh Đăng, Quận 3, Tp. Hồ Chí Minh{" "}
+            </p>
+            <p>
+              <b>HCM: </b>Tầng 7 - Toà Nha Q. Ninh Kiều, TP. Cần Thơ
+            </p>
+            <p>
+              <a href="www.facebook.com">
+                <FacebookFilled
+                  style={{ fontSize: "30px", color: "#4267b2" }}
+                />
+              </a>
+
+              <a href="www.linkedin.com">
+                <LinkedinFilled
+                  style={{ fontSize: "30px", color: "#0958d9" }}
+                />
+              </a>
+
+              <a href="wwww.instagram.com">
+                <InstagramFilled
+                  style={{ fontSize: "30px", color: "#E95950" }}
+                />
+              </a>
+            </p>
+          </ul>
+
+          <ul>
+            <b> Bạn cần trợ giúp? Hãy gọi ngay</b>
+            <p>
+              <a href="#">1900 2045 </a>
+            </p>
+          </ul>
         </div>
-        
-        <div classname = "contact">
-          <ul> <b> Bạn cần trợ giúp? Hãy gọi ngay</b></ul>
-          <div style ={{ color : "#faad14", fontSize :"30px"}}>1900 2045 </div>
+        <div>
+          <FloatButton
+            icon={<QuestionCircleFilled />}
+            style={{ fontSize: "10px" }}
+          />
         </div>
- 
-  </div>
-   </>
-  
-    )
+      </div>
+    </>
+    /*style ={{ }}*/
+  );
 }
 
-
-export default Footer
- //*<FacebookFilled style={{ fontSize: '30px', color :"grid"}} />*//
+export default Footer;
+//*<FacebookFilled style={{ fontSize: '30px', color :"grid"}} />*//
