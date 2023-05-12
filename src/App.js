1964f8fb267065bbd5eef5b020c00aba7151bf17
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-12 13:08:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,96 +1,15 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Home from './pages/home/Home';
-/*import { Layout, Space } from 'antd';*/
-
-
 
 function App() {
- // const footerStyle: React.CSSProperties = {
-  //  textAlign: 'center',
-   // color: '#fff',
-   // backgroundColor: '#7dbcea',
-  //};
   return (
     <>
       <Header />
-      <main>
-      </main>
+      <main></main>
       <Footer />
     </>
-  )
+  );
 }
-  export default App
- /*const { Header, Footer, Sider, Content } = Layout;
-  const headerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 50,
-    lineHeight: '64px',
-    backgroundColor: '#7dbcea',
-  };
-  
-  const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-    minHeight: 120,
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#108ee9',
-  };
-  
-  const siderStyle: React.CSSProperties = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#3ba0e9',
-  };
-  
-  const footerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#7dbcea',
-  };
-  /*return (
-  <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
-    <Layout>
-      <Header style={headerStyle}>Header</Header>
-      <Content style={contentStyle}>Content</Content>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-    <Layout>
-      <Header style={headerStyle}>Header</Header>
-      <Layout>
-        <Sider style={siderStyle}>Sider</Sider>
-        <Content style={contentStyle}>Content</Content>
-      </Layout>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-    <Layout>
-      <Header style={headerStyle}>Header</Header>
-      <Layout>
-        <Content style={contentStyle}>Content</Content>
-        <Sider style={siderStyle}>Sider</Sider>
-      </Layout>
-      <Footer style={footerStyle}>Footer</Footer>
-    </Layout>
-    <Layout>
-      <Sider style={siderStyle}>Sider</Sider>
-      <Layout>
-        <Header style={headerStyle}>Header</Header>
-        <Content style={contentStyle}>Content</Content>
-        <Footer style={footerStyle}>Footer</Footer>
-      </Layout>
-    </Layout>
-  </Space>
-);
-  }*/
- 
-/*function App() {
-  return (
-    <div className="App">
-        <Button type="primary">Hello gà coder</Button>
-      </div>
-);
-}*/
+export default App;
+
