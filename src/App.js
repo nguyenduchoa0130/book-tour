c55dfd1762d8f9b8a000f3e5772fa08efa8e6908
nguyenduchoa0130 <nguyenduchoa0130@gmail.com>
2023-05-13 09:47:04 +0700

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
+import Tours from './pages/tours/Tours';
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route path='/tours' element={<Tours />} />
+          <Route path='/hotel' element={<Hotel />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
