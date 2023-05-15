4b0b1591860833965dbd95f4863a9a9a5b37e8cd
Sarah2k1 <thuylinhnd.bk@gmail.com>
2023-05-16 02:01:33 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles.css';
-import { NavLink } from 'react-router-dom';
 /*import { Layout, Menu, Avatar, Badge, Space } from 'antd';
 import { UserOutlined, PhoneOutlined } from '@ant-design/icons';
 import logo from './public/logo192.png';
@@ -8,44 +7,42 @@ const { Header} = Layout;*/
 
 function Header() {
   return (
-    <div className='header'>
-      <div className='logo'>
-        <NavLink to='/'>
-          <img src='https://res.ivivu.com/hotel/img/logo-2023n.svg' alt='ivivu' />
-        </NavLink>
+    <div className="header">
+      <div className="logo">
+        <img src="https://res.ivivu.com/hotel/img/logo-2023n.svg" alt="ivivu" />
       </div>
-      <div className='menu'>
+      <div className="menu">
         <ul>
           <li>
-            <NavLink to='/hotel'>Khách sạn</NavLink>
+            <a href="#">Khách sạn</a>
           </li>
           <li>
-            <NavLink to='/tours'>Tours</NavLink>
+            <a href="#">Tours</a>
           </li>
           <li>
-            <a href='!#'>Vé máy bay</a>
+            <a href="#">Vé máy bay</a>
           </li>
           <li>
-            <a href='!#'>Cẩm nang du lịch</a>
+            <a href="#">Cẩm nang du lịch</a>
           </li>
           <li>
-            <a href='!#'>More</a>
+            <a href="#">More</a>
           </li>
         </ul>
       </div>
 
-      <div className='account'>
-        <div className='avatar'>
-          <img
-            src='https://res.ivivu.com/hotel/img/avatars/avatar-default-white.svg'
-            alt='avatar'
-          />
-        </div>
-        <div className='signin'>Tài khoản</div>
+      <div className="account">
+          <div className="avatar">
+             <img src="https://res.ivivu.com/hotel/img/avatars/avatar-default-white.svg" alt="avatar" />  
+             <button className="taikhoan">Tài khoản</button>
+             </div>
+            
       </div>
-      <div className='contact'>1900 2045</div>
-    </div>
+      <div className="contact">1900 2045</div>
+      
+  </div>
   );
 }
 
 export default Header;
+
