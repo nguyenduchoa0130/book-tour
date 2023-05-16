a8ca24ad0058b5904eb4a4bc9d7d642062d769ca
Sarah2k1 <thuylinhnd.bk@gmail.com>
2023-05-16 11:07:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom'
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Home from './pages/home/Home';
-import Hotel from './pages/hotel/Hotel';
-import Tours from './pages/tours/Tours';
+
+import Register from './pages/register/Register';
+
+/*import { Layout, Space } from 'antd';*/
+
+
 
 function App() {
+ // const footerStyle: React.CSSProperties = {
+  //  textAlign: 'center',
+   // color: '#fff',
+   // backgroundColor: '#7dbcea',
+  //};
   return (
     <>
       <Header />
       <main>
         <Routes>
-          <Route path='/tours' element={<Tours />} />
-          <Route path='/hotel' element={<Hotel />} />
-          <Route path='/' element={<Home />} />
+           <Route path='/register' element={<Register />} />
         </Routes>
       </main>
       <Footer />
     </>
-  );
+  )
 }
-export default App;
-
+  export default App
+ /*const { Header, Footer, Sider, Content } = Layout;
+  const headerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 50,
+    lineHeight: '64px',
+    backgroundColor: '#7dbcea',
+  };
+  
+  const contentStyle: React.CSSProperties = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#108ee9',
+  };
+  
+  const siderStyle: React.CSSProperties = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#3ba0e9',
+  };
+  
+  const footerStyle: React.CSSProperties = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#7dbcea',
+  };
+  /*return (
+  <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider style={siderStyle}>Sider</Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Content style={contentStyle}>Content</Content>
+        <Sider style={siderStyle}>Sider</Sider>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Sider style={siderStyle}>Sider</Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
+  </Space>
+);
+  }*/
+ 
+/*function App() {
+  return (
+    <div className="App">
+        <Button type="primary">Hello gà coder</Button>
+      </div>
+);
+}*/
