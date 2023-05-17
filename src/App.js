af6d767a9f9fc4d225ff4ddf8d5e5565ac918531
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-17 21:36:53 +0700

    loh=gin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-
+import Login from './pages/home/login/Login'; 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+      <Login/>
+      </main>
       <Footer />
     </>
   );
