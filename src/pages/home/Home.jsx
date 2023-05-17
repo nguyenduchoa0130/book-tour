993c58ebdf9ce2fd5436cc8be6abca060e1f4f9b
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-05-17 23:29:49 +0700

    config code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './styles.css'
-import video from './Assets/.beach-153167.mp4'
+import './styles.css';
+import video from '../../Assets/beach-153167.mp4';
 const Home = () => {
   return (
     <section className='home'>
-       <div className='overlay'></div>
-       <video src={video} muted autoPlay loop type="video/mp4"></video>
+      <div className='overlay'></div>
+      <video src={video} muted autoPlay loop type='video/mp4'></video>
     </section>
-    
   );
 };
 
 export default Home;
+
