65bc0fd3750b58917a66d7ea88f642456b30ba06
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-18 17:52:00 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Login from './pages/home/login/Login'; 
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+import React from "react";
+import { BrowserRouter } from 'react-router-dom';
+
+ 
 function App() {
   return (
     <>
       <Header />
       <main>
-      <Login/>
+        <BrowserRouter>
+        <Routes>
+          <Route path='login' element = {<Login/>}/>
+          <Route path='/home' element = {<Home/>}/>
+        </Routes>
+        </BrowserRouter>
       </main>
       <Footer />
     </>
