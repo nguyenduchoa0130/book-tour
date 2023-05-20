491b2eb50d6ee3c6aefed9ee8cb13988fadeb8c4
Sarah2k1 <thuylinhnd.bk@gmail.com>
2023-05-21 00:51:47 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,7 +22,12 @@ function Header() {
             <a href='!#'>Cẩm nang du lịch</a>
           </li>
           <li>
-            <a href='!#'>More</a>
+            <a href='!#'>   More   </a>
+            <ul class="subnav">
+                 <li><a href="">Giới thiệu</a></li>
+                 <li><a href="">Hỏi đáp</a></li>
+                 <li><a href="">Hỗ trợ</a></li>        
+            </ul>
           </li>
         </ul>
       </div>
@@ -33,9 +38,10 @@ function Header() {
             src='https://res.ivivu.com/hotel/img/avatars/avatar-default-white.svg'
             alt='avatar'
           />
-          <button className='taikhoan'>Tài khoản</button>
+          <div className='taikhoan'>Tài khoản</div>
         </div>
       </div>
+
       <div className='contact'>1900 2045</div>
     </div>
   );
