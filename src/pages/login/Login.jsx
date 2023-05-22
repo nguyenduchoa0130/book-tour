1ca448d872089ac6b17eb694440af36fa049e7ec
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-23 01:05:30 +0700

    moi

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,8 @@
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './styles.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 function Login() {
   const onFinish = values => {
@@ -9,57 +10,9 @@ function Login() {
   };
   return (
     <>
-<<<<<<< HEAD
       <div className="login">
         <div className="card">
-          <div className="card-body">
-            <h1>Login</h1>
-            <Form
-              name="normal_login"
-              className="login-form"
-              initialValues={{
-                remember: true
-              }}
-              onFinish={onFinish}>
-              <Form.Item
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your Username!'
-                  }
-                ]}>
-                <Input
-                  prefix={<UserOutlined className="site-form-item-icon" />}
-                  placeholder="Username"
-                />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your Password!'
-                  }
-                ]}>
-                <Input
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <a
-                  className="login-form-forgot"
-                  href=""
-=======
-      <div className="login-container">
-        <div className="login">
-          <h1>Login</h1>
+          <h1 style={{ marginTop: '50px', fontFamily: 'VNI-Pagon' }}>Login</h1>
           <Form
             name="normal_login"
             className="login-form"
@@ -74,7 +27,10 @@ function Login() {
                   required: true,
                   message: 'Please input your Username!'
                 }
-              ]}>
+              ]}
+              style={{ width: '500px', margin: '0 auto', marginTop: '20px' }} // Chỉnh độ rộng của trường username
+              className="text-center" // Căn giữa trường username- khong duoc
+            >
               <Input
                 prefix={<UserOutlined className="site-form-item-icon" />}
                 placeholder="Username"
@@ -87,7 +43,8 @@ function Login() {
                   required: true,
                   message: 'Please input your Password!'
                 }
-              ]}>
+              ]}
+              style={{ width: '500px', margin: '0 auto', marginTop: '20px', marginBottom: '20px' }}>
               <Input
                 prefix={<LockOutlined className="site-form-item-icon" />}
                 type="password"
@@ -101,10 +58,11 @@ function Login() {
 
               <a
                 className="login-form-forgot"
-                href="!#"
+                href=""
                 style={{
                   color: '#1f1f1f',
-                  fontWeight: 'bolder'
+                  fontWeight: 'bolder',
+                  marginLeft: '300px' // Khoảng cách trái
                 }}>
                 Forgot password
               </a>
@@ -115,30 +73,10 @@ function Login() {
                   type="primary"
                   htmlType="submit"
                   className="login-form-button"
-                  style={{ color: '#000000' }}>
+                  style={{ color: '#000000', marginBottoms: '20px' }}>
                   Log in
                 </Button>
-                Or{' '}
-                <a
-                  href="!#"
->>>>>>> e880b7af40a14972caf1d7e0894189c49d53afd5
-                  style={{
-                    color: '#1f1f1f',
-                    fontWeight: 'bolder'
-                  }}>
-<<<<<<< HEAD
-                  Forgot password
-                </a>
-              </Form.Item>
-              <div className="button">
-                <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className="login-form-button"
-                    style={{ color: '#000000' }}>
-                    Log in
-                  </Button>
+                <p style={{ marginBottom: '20px', marginTop: '20px' }}>
                   Or{' '}
                   <a
                     href=""
@@ -148,17 +86,30 @@ function Login() {
                     }}>
                     Register now!
                   </a>
-                </Form.Item>
-              </div>
-            </Form>
-          </div>
-=======
-                  Register now!
-                </a>
+                </p>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      className="login-form-button"
+                      style={{ color: '#000000' }}>
+                      Continue with Facebook
+                    </Button>
+                  </Form.Item>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      className="login-form-button"
+                      style={{ color: '#000000' }}>
+                      Continue with Google
+                    </Button>
+                  </Form.Item>
+                </div>
               </Form.Item>
             </div>
           </Form>
->>>>>>> e880b7af40a14972caf1d7e0894189c49d53afd5
         </div>
       </div>
     </>
