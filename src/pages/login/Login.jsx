2f7ecad2b5da275938f966bbdc1a1f785493fb1e
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-22 23:53:38 +0700

    moi

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -11,77 +11,81 @@ function Login() {
   return (
     <>
       <div className="login">
-        <h1>Login</h1>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{
-            remember: true
-          }}
-          onFinish={onFinish}>
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Username!'
-              }
-            ]}>
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Username"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Password!'
-              }
-            ]}>
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
+        <div className="card">
+          <div className="card-body">
+            <h1>Login</h1>
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{
+                remember: true
+              }}
+              onFinish={onFinish}>
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Username!'
+                  }
+                ]}>
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Username"
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Password!'
+                  }
+                ]}>
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
 
-            <a
-              className="login-form-forgot"
-              href=""
-              style={{
-                color: '#1f1f1f',
-                fontWeight: 'bolder'
-              }}>
-              Forgot password
-            </a>
-          </Form.Item>
-          <div className="button">
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-                style={{ color: '#000000' }}>
-                Log in
-              </Button>
-              Or{' '}
-              <a
-                href=""
-                style={{
-                  color: '#1f1f1f',
-                  fontWeight: 'bolder'
-                }}>
-                Register now!
-              </a>
-            </Form.Item>
+                <a
+                  className="login-form-forgot"
+                  href=""
+                  style={{
+                    color: '#1f1f1f',
+                    fontWeight: 'bolder'
+                  }}>
+                  Forgot password
+                </a>
+              </Form.Item>
+              <div className="button">
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                    style={{ color: '#000000' }}>
+                    Log in
+                  </Button>
+                  Or{' '}
+                  <a
+                    href=""
+                    style={{
+                      color: '#1f1f1f',
+                      fontWeight: 'bolder'
+                    }}>
+                    Register now!
+                  </a>
+                </Form.Item>
+              </div>
+            </Form>
           </div>
-        </Form>
+        </div>
       </div>
     </>
   );
