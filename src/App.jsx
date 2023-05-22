1347d13aef3cd7b207a94fdcccc470735889f6ec
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-05-22 23:30:56 +0700

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </main>
       <Footer />
@@ -20,4 +21,3 @@ function App() {
   );
 }
 export default App;
-
