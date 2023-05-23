8a7216b78199a40093bad8ce31085e4837ccbdb8
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-05-23 14:31:41 +0700

    update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './styles.css';
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-
+import mountain from '../../Assets/mountain.mp4';
 function Login() {
   const onFinish = values => {
     console.log('Received values of form: ', values);
   };
   return (
     <>
-      <div className="login">
+      <div className="login" style={{ position: 'relative' }}>
+        <video className="background-video" autoPlay loop muted>
+          <source src={mountain} type="video/mp4" />
+        </video>
+
         <div className="card">
-          <h1 style={{ marginTop: '50px', fontFamily: 'VNI-Pagon' }}>Login</h1>
+          <h1
+            style={{
+              marginTop: '50px',
+              fontFamily: 'VNI-Pagon',
+              color: '',
+              fontFamily: 'VNI-Pagon'
+            }}>
+            Login
+          </h1>
+
           <Form
             name="normal_login"
             className="login-form"
@@ -28,7 +40,12 @@ function Login() {
                   message: 'Please input your Username!'
                 }
               ]}
-              style={{ width: '500px', margin: '0 auto', marginTop: '20px' }} // Chỉnh độ rộng của trường username
+              style={{
+                width: '500px',
+                margin: '0 auto',
+                marginTop: '20px',
+                color: ''
+              }} // Chỉnh độ rộng của trường username
               className="text-center" // Căn giữa trường username- khong duoc
             >
               <Input
@@ -73,7 +90,7 @@ function Login() {
                   type="primary"
                   htmlType="submit"
                   className="login-form-button"
-                  style={{ color: '#000000', marginBottoms: '20px' }}>
+                  style={{ color: '#000000', marginBottoms: '20px', fontFamily: 'VNI-Pagon' }}>
                   Log in
                 </Button>
                 <p style={{ marginBottom: '20px', marginTop: '20px' }}>
@@ -93,7 +110,7 @@ function Login() {
                       type="primary"
                       htmlType="submit"
                       className="login-form-button"
-                      style={{ color: '#000000' }}>
+                      style={{ color: '#000000', fontFamily: 'VNI-Pagon' }}>
                       Continue with Facebook
                     </Button>
                   </Form.Item>
@@ -102,7 +119,7 @@ function Login() {
                       type="primary"
                       htmlType="submit"
                       className="login-form-button"
-                      style={{ color: '#000000' }}>
+                      style={{ color: '#000000', fontFamily: 'VNI-Pagon' }}>
                       Continue with Google
                     </Button>
                   </Form.Item>
