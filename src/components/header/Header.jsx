cf0ce5146b2fbdb8c5cb01371d8752825b9fac41
Sarah2k1 <thuylinhnd.bk@gmail.com>
2023-06-18 13:13:39 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import {BsFillTelephoneFill} from 'react-icons/bs'
 
 function Header() {
   return (
@@ -21,12 +22,13 @@ function Header() {
           <li>
             <a href='!#'>Cẩm nang du lịch</a>
           </li>
-          <li>
-            <a href='!#'>More</a>
-            <ul class="subnav">
-                 <li><a href="">Giới thiệu</a></li>
-                 <li><a href="">Hỏi đáp</a></li>
-                 <li><a href="">Hỗ trợ</a></li>        
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">More
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                 <li><a href="#">Giới thiệu</a></li>
+                 <li><a href="#">Hỏi đáp</a></li>
+                 <li><a href="#">Hỗ trợ</a></li>        
             </ul>
           </li>
         </ul>
@@ -38,25 +40,16 @@ function Header() {
             src='https://res.ivivu.com/hotel/img/avatars/avatar-default-white.svg'
             alt='avatar'
           />
-          <div className='taikhoan'>
-          Tài khoản
-          <ul class="user-account">
-             <li class="btn-login-header">
-             <btn type="button" class="btn-login">Đăng nhập</btn>
-             </li>
-             <li class="register-text">
-             "Chưa có tài khoản? "
-             <span class="register-link">Đăng ký </span>    
-             ngay       
-             </li>
-          </ul>
-        
-          </div>
+          <span>Sign in</span>
         </div>
       </div>
 
-      <div className='contact'>1900 2045</div>
+      <div className='contact flex'>
+        <BsFillTelephoneFill className="icon"/>
+        <span>1900 2045</span>  
     </div>
+    </div>
+    
   );
 }
 
