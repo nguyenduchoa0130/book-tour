9b08af3c8c77b47f1794008a68cebec0cb49a6a9
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-07-10 21:02:33 +0700

    update UI

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,67 +10,64 @@ const LoginForm = () => {
   const {
     handleSubmit,
     register,
-    formState: { errors }
+    formState: { errors },
   } = useForm();
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log('Submitted data:', data);
   };
 
   return (
     <div className={styles['login-form-container']}>
-      <video autoPlay muted loop className={styles['background-video']}>
-        <source src="/assets/audio/beach.mp4" type="video/mp4" />
-      </video>
       <Form
-        name="loginForm"
-        layout="vertical"
+        name='loginForm'
+        layout='vertical'
         onFinish={handleSubmit(onSubmit)}
         initialValues={{ remember: true }}
         className={styles['login-form']}>
-        <Typography.Title level={1} className="text-center py-3 text-uppercase">
+        <Typography.Title level={1} className='text-center py-3 text-uppercase'>
           đăng nhập
         </Typography.Title>
         <Form.Item
-          label="Tên đăng nhập"
-          name="username"
+          label='Tên đăng nhập'
+          name='username'
           rules={[{ required: true, message: 'Tên đăng nhập không được để trống!' }]}
           validateStatus={errors.username ? 'error' : ''}
           help={errors.username && errors.username.message}>
           <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Tên đăng nhập"
-            size="large"
+            prefix={<UserOutlined className='site-form-item-icon' />}
+            placeholder='Tên đăng nhập'
+            size='large'
             {...register('username')}
           />
         </Form.Item>
         <Form.Item
-          label="Mật khẩu"
-          name="password"
+          label='Mật khẩu'
+          name='password'
           rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
           validateStatus={errors.password ? 'error' : ''}
           help={errors.password && errors.password.message}>
           <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Mật khẩu"
+            prefix={<LockOutlined className='site-form-item-icon' />}
+            type='password'
+            placeholder='Mật khẩu'
             {...register('password')}
-            size="large"
+            size='large'
           />
         </Form.Item>
         <Form.Item
-          label="Vai trò"
-          name="role"
+          label='Vai trò'
+          name='role'
           rules={[{ required: true, message: 'Vui lòng nhập vai trò của bạn!' }]}
           validateStatus={errors.role ? 'error' : ''}
           help={errors.role && errors.role.message}>
-          <Select placeholder="Chọn vai trò" allowClear {...register('role')}>
-            <Option value="admin">Admin</Option>
-            <Option value="user">User</Option>
+          <Select placeholder='Chọn vai trò' allowClear {...register('role')}>
+            <Option value='admin'>Admin</Option>
+            <Option value='user'>User</Option>
           </Select>
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit" className="w-100" size="large">
+          <Button type='primary' htmlType='submit' className='w-100' size='large'>
             Gửi
           </Button>
         </Form.Item>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -8,42 +8,37 @@ const Register = () => {
   const {
     handleSubmit,
     control,
-    formState: { errors }
+    formState: { errors },
   } = useForm();
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
     <div className={styles['register-container']}>
-      <div className={styles['video-background']}>
-        <video autoPlay loop muted className={styles.video}>
-          <source src="/assets/audio/ocean.mp4" type="video/mp4" />
-        </video>
-      </div>
       <div className={styles['form-container']}>
-        <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
-          <Typography.Title level={1} className="text-center py-3 text-uppercase">
+        <Form layout='vertical' onFinish={handleSubmit(onSubmit)}>
+          <Typography.Title level={1} className='text-center py-3 text-uppercase'>
             đăng ký tài khoản
           </Typography.Title>
           <Form.Item
-            label="Tên đăng nhập"
-            name="username"
+            label='Tên đăng nhập'
+            name='username'
             rules={[{ required: true, message: 'Tên đăng nhập không được để trống!' }]}>
-            <Input placeholder="Tên đăng nhập" prefix={<UserOutlined />} size="large" />
+            <Input placeholder='Tên đăng nhập' prefix={<UserOutlined />} size='large' />
           </Form.Item>
 
           <Form.Item
-            label="Mật khẩu"
-            name="password"
+            label='Mật khẩu'
+            name='password'
             rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}>
-            <Input.Password placeholder="Mật khẩu" prefix={<LockOutlined />} size="large" />
+            <Input.Password placeholder='Mật khẩu' prefix={<LockOutlined />} size='large' />
           </Form.Item>
 
           <Form.Item
-            label="Nhập lại mật khẩu"
-            name="confirmPassword"
+            label='Nhập lại mật khẩu'
+            name='confirmPassword'
             rules={[
               { required: true, message: 'Vui lòng xác nhận lại mật khẩu' },
               ({ getFieldValue }) => ({
@@ -52,30 +47,30 @@ const Register = () => {
                     return Promise.resolve();
                   }
                   return Promise.reject('Mật khẩu không khớp');
-                }
-              })
+                },
+              }),
             ]}>
             <Input.Password
               prefix={<LockOutlined />}
-              size="large"
-              placeholder="Nhập lại mật khẩu"
+              size='large'
+              placeholder='Nhập lại mật khẩu'
             />
           </Form.Item>
 
-          <Form.Item label="Họ và tên" name="fullName" rules={[{ required: false }]}>
-            <Input prefix={<UserOutlined />} size="large" placeholder="Họ và tên" />
+          <Form.Item label='Họ và tên' name='fullName' rules={[{ required: false }]}>
+            <Input prefix={<UserOutlined />} size='large' placeholder='Họ và tên' />
           </Form.Item>
 
-          <Form.Item label="Địa chỉ" name="address" rules={[{ required: false }]}>
-            <Input.TextArea rows={4} size="large" placeholder="Địa chỉ" />
+          <Form.Item label='Địa chỉ' name='address' rules={[{ required: false }]}>
+            <Input.TextArea rows={4} size='large' placeholder='Địa chỉ' />
           </Form.Item>
 
-          <Form.Item label="Số điện thoại" name="phoneNumber" rules={[{ required: false }]}>
-            <Input prefix={<PhoneOutlined />} size="large" placeholder="Số điện thoại" />
+          <Form.Item label='Số điện thoại' name='phoneNumber' rules={[{ required: false }]}>
+            <Input prefix={<PhoneOutlined />} size='large' placeholder='Số điện thoại' />
           </Form.Item>
 
           <Form.Item>
-            <Button type="primary" htmlType="submit" size="large" className="w-100">
+            <Button type='primary' htmlType='submit' size='large' className='w-100'>
               Đăng ký
             </Button>
           </Form.Item>
